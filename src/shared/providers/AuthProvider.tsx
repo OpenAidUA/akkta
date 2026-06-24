17aba7a10489e610ec39e4a92c38cc639b60a909
Roman Kozyrev <r.prohor7@gmail.com>
2026-06-25 00:00:28 +0200

    fixed meta data

--- src/shared/providers/AuthProvider.tsx
+++ src/shared/providers/AuthProvider.tsx
@@ -24,6 +24,10 @@ export function AuthProvider({
   const [user, setUser] = useState<User | null>(initialUser);
   const router = useRouter();
 
+  useEffect(() => {
+    setUser(initialUser);
+  }, [initialUser]);
+
   // Keep user sync with supabase auth state
   useEffect(() => {
     const {
